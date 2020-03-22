17cc8b5b41f3630f0d786e5a0e5bead59ff52428
postdan <postdan@yahoo.com>
2020-03-22 20:57:12 +0200

    finish

---
+++ Lab8_Fibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab8_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci");
+        }
+    }
+}
