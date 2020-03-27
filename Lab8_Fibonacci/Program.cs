351e0c820a838bbbe44e4a514841a2443cac823d
postdan <postdan@yahoo.com>
2020-03-27 16:58:22 +0200

    commit

--- Lab8_Fibonacci/Program.cs
+++ Lab8_Fibonacci/Program.cs
@@ -6,7 +6,30 @@ namespace Lab8_Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fibonacci");
+            Console.Write("For Fibonacci sequence, insert N:");
+            bool isNumber = Int32.TryParse(Console.ReadLine(), out int n);
+            ulong fiboN = 0;
+            ulong n1 = 1;
+            ulong n2 = 0;
+            int i = 2;
+            Console.WriteLine($"For n={n}, Fibonacci sequence is:");
+            if (n == 0)
+            {
+                Console.WriteLine($"F(0) = 0");
+            }
+                if (n > 0)
+            {
+                Console.WriteLine($"F(0) = 0");
+                Console.WriteLine($"F(1) = 1");
+            }
+            while (i<=n)
+            {
+                fiboN = n2 + n1;
+                n2 = n1;
+                n1 = fiboN;
+                Console.WriteLine($"F({i}) = {fiboN}");
+                i++;
+            }
         }
     }
 }
